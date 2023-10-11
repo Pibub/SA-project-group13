48edb4ff01cee87ee013d45428ee68e31267b540
Pibub <thanapat.suth@ku.th>
2023-10-11 17:38:48 +0700

    add ListView FXML

--- src/main/java/ku/cs/services/DatabaseConnection.java
+++ src/main/java/ku/cs/services/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DatabaseConnection {
     public Connection databaseLink;
     public Connection getConnection(){
-        String databaseName = "warehouse";
+        String databaseName = "test";
         String databaseUser = "root";
         String databasePassword = "";
         String url = "jdbc:mysql://localhost:3306/" + databaseName;
